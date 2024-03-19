cda2e26402f4bd68862b9f3bc8294e524b74f1e9
AdamTij <163690825+AdamTij@users.noreply.github.com>
2024-03-19 00:30:56 -0700

    Upload website code
    Upload front and back-end code that interacts with our database

---
+++ Milestone4/hello.php
@@ -0,0 +1,48 @@
+<?php
+$servername = "cssql.seattleu.edu";
+$username = "bd_atidjani";
+$password = "8AN9QY2G";
+$dbname = "bd_atidjani";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+die("Connection failed: " . mysqli_connect_error());
+}
+
+$query = $_POST["query"];
+$action = $_POST["action"];
+$sql = $action . " " . $query;
+
+$result = mysqli_query($conn, $sql);
+
+if (!$conn) {
+die ("Query was not successful");
+}
+
+if ($action == "select") {
+
+    if (mysqli_num_rows($result) > 0) {
+        echo "<table border = '1'>\n";
+        // output data of each row
+        while ($row = mysqli_fetch_row($result)) {
+            echo "<tr>\n";
+            $cols = mysqli_num_fields($result);
+            for ($i = 0; $i < $cols; $i++) {
+                echo "<td>" . $row[$i] . "</td>\n";
+            }
+            echo "</tr>\n";
+        }
+        echo "</table>\n";
+    } else {
+        echo "0 results";
+    }
+    mysqli_free_result($result);
+}
+else{
+    echo $action . " was successful";
+}
+
+
+mysqli_close($conn);
